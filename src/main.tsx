61869e11446da00f0e67c6ca2ab43f014beb7ac3
safvan2825 <safvan2825@gmail.com>
2026-08-11 23:26:05 +0530

    Load examination workspace layout fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './design-overrides.css';
+import './examination-workspace-fix.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
